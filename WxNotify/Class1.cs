b2de3e9e54761f58f967c8e06893423393fdc7c3
zhangtianqing <a1092484359@yeah.net>
2023-10-12 15:24:04 +0800

    提交初版

---
+++ WxNotify/Class1.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dll.Utils
+{
+    public class WxNotify
+    {
+         static  string url = "http://localhost:3001/webhook/msg"; // 替换为实际的 API 地址
+        public static void SendMsg(string userNickName, string msg) {
+            string json = "{ \"to\":\""+ userNickName + "\",\"type\":\"text\",\"content\":\""+msg+"\"}";
+            Post(url, json);
+        }
+        public static string Post(string Url, string jsonParas)
+        {
+            string strURL = Url;
+            //创建一个HTTP请求  
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(strURL);
+            //Post请求方式  
+            request.Method = "POST";
+            //内容类型
+            request.ContentType = "application/json";
+            //设置参数，并进行URL编码 
+            string paraUrlCoded = jsonParas;//System.Web.HttpUtility.UrlEncode(jsonParas);   
+            byte[] payload;
+            //将Json字符串转化为字节  
+            payload = System.Text.Encoding.UTF8.GetBytes(paraUrlCoded);
+            //设置请求的ContentLength   
+            request.ContentLength = payload.Length;
+            //发送请求，获得请求流 
+            Stream writer;
+            try
+            {
+                writer = request.GetRequestStream();//获取用于写入请求数据的Stream对象
+            }
+            catch (Exception)
+            {
+                writer = null;
+                Console.Write("连接服务器失败!");
+            }
+            //将请求参数写入流
+            writer.Write(payload, 0, payload.Length);
+            writer.Close();//关闭请求流
+                           // String strValue = "";//strValue为http响应所返回的字符流
+            HttpWebResponse response;
+            try
+            {
+                //获得响应流
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException ex)
+            {
+                response = ex.Response as HttpWebResponse;
+            }
+            Stream s = response.GetResponseStream();
+            //  Stream postData = Request.InputStream;
+            StreamReader sRead = new StreamReader(s);
+            string postContent = sRead.ReadToEnd();
+            sRead.Close();
+            return postContent;//返回Json数据
+        }
+
+         class Obj
+        {
+            public string to;
+            public string type;
+            public string content;
+        }
+    }
+}
